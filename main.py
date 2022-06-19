63a38e21e1d95bc70fc79b0d9bc5d511f5fb914e
Suman Achary <suman.achary95@gmail.com>
2022-06-19 17:54:22 +0530

    updated

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ Date: 18-06-2022
 Project:
 '''
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
